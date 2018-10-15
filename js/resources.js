6d468ce38e5bdc9768dee1c34f331fa8c364c0fe
PK007PK <32392879+PK007PK@users.noreply.github.com>
2018-10-15 10:34:23 +0200

    More content added (numbers 11 - 20).

--- js/resources.js
+++ js/resources.js
@@ -1,43 +1,127 @@
 // JavaScript Document
 
 let owoce = [
-	
-	[
-	{ nazwa: 'gruszka', name : 'pear', image: 'images/fruits/pear.svg' },
-	{ nazwa: 'jabłko', name : 'apple', image: 'images/fruits/apple.svg' }, 
-	{ nazwa: 'banan', name : 'banana', image: 'images/fruits/banana.svg' }, 
-	{ nazwa: 'śliwka', name : 'plum', image: 'images/fruits/plum.svg' }, 
-	{ nazwa: 'cytryna', name : 'lemon', image: 'images/fruits/lemon.svg' }, 
-	{ nazwa: 'winogrono', name : 'grape', image: 'images/fruits/grape.svg' }, 
-	{ nazwa: 'truskawka', name : 'strawberry', image: 'images/fruits/strawberry.svg' }, 
-	{ nazwa: 'malina', name : 'raspberry', image: 'images/fruits/raspberry.svg' }, 
-	{ nazwa: 'wiśnia', name : 'cherry', image: 'images/fruits/cherry.svg' }, 
-	{ nazwa: 'jagoda', name : 'blueberry', image: 'images/fruits/blueberry.svg' }, 
+
+	[{
+			nazwa: 'gruszka',
+			name: 'pear',
+			image: 'images/fruits/pear.svg'
+		},
+		{
+			nazwa: 'jabłko',
+			name: 'apple',
+			image: 'images/fruits/apple.svg'
+		},
+		{
+			nazwa: 'banan',
+			name: 'banana',
+			image: 'images/fruits/banana.svg'
+		},
+		{
+			nazwa: 'śliwka',
+			name: 'plum',
+			image: 'images/fruits/plum.svg'
+		},
+		{
+			nazwa: 'cytryna',
+			name: 'lemon',
+			image: 'images/fruits/lemon.svg'
+		},
+		{
+			nazwa: 'winogrono',
+			name: 'grape',
+			image: 'images/fruits/grape.svg'
+		},
+		{
+			nazwa: 'truskawka',
+			name: 'strawberry',
+			image: 'images/fruits/strawberry.svg'
+		},
+		{
+			nazwa: 'malina',
+			name: 'raspberry',
+			image: 'images/fruits/raspberry.svg'
+		},
+		{
+			nazwa: 'wiśnia',
+			name: 'cherry',
+			image: 'images/fruits/cherry.svg'
+		},
+		{
+			nazwa: 'jagoda',
+			name: 'blueberry',
+			image: 'images/fruits/blueberry.svg'
+		},
 	],
 
-	[
-	{ nazwaZbioru: 'Owoce', opis: 'Owoce', polecenie: 'Poznaj nazwy owoców', image: 'images/fruits.jpg'},
-	]
+	[{
+		nazwaZbioru: 'Owoce',
+		opis: 'Owoce',
+		polecenie: 'Poznaj nazwy owoców',
+		image: 'images/fruits.jpg'
+	}, ]
 ];
 
 let kolory = [
-	
-	[
-	{ nazwa: 'biały', name : 'white', image: 'images/colours/white.svg' },
-	{ nazwa: 'czarny', name : 'black', image: 'images/colours/black.svg' }, 
-	{ nazwa: 'szary', name : 'gray', image: 'images/colours/gray.svg' }, 
-	{ nazwa: 'czerwony', name : 'red', image: 'images/colours/red.svg' }, 
-	{ nazwa: 'zielony', name : 'green', image: 'images/colours/green.svg' }, 
-	{ nazwa: 'niebieski', name : 'blue', image: 'images/colours/blue.svg' }, 
-	{ nazwa: 'żółty', name : 'yellow', image: 'images/colours/yellow.svg' }, 
-	{ nazwa: 'brązowy', name : 'brown', image: 'images/colours/brown.svg' }, 
-	{ nazwa: 'różowy', name : 'pink', image: 'images/colours/pink.svg' }, 
-	{ nazwa: 'pomarańczowy', name : 'orange', image: 'images/colours/orange.svg' },
+
+	[{
+			nazwa: 'biały',
+			name: 'white',
+			image: 'images/colours/white.svg'
+		},
+		{
+			nazwa: 'czarny',
+			name: 'black',
+			image: 'images/colours/black.svg'
+		},
+		{
+			nazwa: 'szary',
+			name: 'gray',
+			image: 'images/colours/gray.svg'
+		},
+		{
+			nazwa: 'czerwony',
+			name: 'red',
+			image: 'images/colours/red.svg'
+		},
+		{
+			nazwa: 'zielony',
+			name: 'green',
+			image: 'images/colours/green.svg'
+		},
+		{
+			nazwa: 'niebieski',
+			name: 'blue',
+			image: 'images/colours/blue.svg'
+		},
+		{
+			nazwa: 'żółty',
+			name: 'yellow',
+			image: 'images/colours/yellow.svg'
+		},
+		{
+			nazwa: 'brązowy',
+			name: 'brown',
+			image: 'images/colours/brown.svg'
+		},
+		{
+			nazwa: 'różowy',
+			name: 'pink',
+			image: 'images/colours/pink.svg'
+		},
+		{
+			nazwa: 'pomarańczowy',
+			name: 'orange',
+			image: 'images/colours/orange.svg'
+		},
 	],
 
-	[
-	{ nazwaZbioru: 'Kolory', opis: 'Kolory', polecenie: 'Naucz się kolorów', image: 'images/colours.jpg' },
-	]
+	[{
+		nazwaZbioru: 'Kolory',
+		opis: 'Kolory',
+		polecenie: 'Naucz się kolorów',
+		image: 'images/colours.jpg'
+	}, ]
 ];
 
 
@@ -46,112 +130,397 @@ let kolory = [
 
 let u2liczby = [
 
-	[
-		{ nazwa: 'zero', name: 'zero', image: 'images/unit2numbers/zero.svg' },
-		{ nazwa: 'jeden', name: 'one', image: 'images/unit2numbers/one.svg' },
-		{ nazwa: 'dwa', name: 'two', image: 'images/unit2numbers/two.svg' },
-		{ nazwa: 'trzy', name: 'three', image: 'images/unit2numbers/three.svg' },
-		{ nazwa: 'cztery', name: 'four', image: 'images/unit2numbers/four.svg' },
-		{ nazwa: 'pięć', name: 'five', image: 'images/unit2numbers/five.svg' },
-		{ nazwa: 'sześć', name: 'six', image: 'images/unit2numbers/six.svg' },
-		{ nazwa: 'siedem', name: 'seven', image: 'images/unit2numbers/seven.svg' },
-		{ nazwa: 'osiem', name: 'eight', image: 'images/unit2numbers/eight.svg' },
-		{ nazwa: 'dziewięć', name: 'nine', image: 'images/unit2numbers/nine.svg' },
-		{ nazwa: 'dziesięć', name: 'ten', image: 'images/unit2numbers/ten.svg' },
+	[{
+			nazwa: 'zero',
+			name: 'zero',
+			image: 'images/unit2numbers/zero.svg'
+		},
+		{
+			nazwa: 'jeden',
+			name: 'one',
+			image: 'images/unit2numbers/one.svg'
+		},
+		{
+			nazwa: 'dwa',
+			name: 'two',
+			image: 'images/unit2numbers/two.svg'
+		},
+		{
+			nazwa: 'trzy',
+			name: 'three',
+			image: 'images/unit2numbers/three.svg'
+		},
+		{
+			nazwa: 'cztery',
+			name: 'four',
+			image: 'images/unit2numbers/four.svg'
+		},
+		{
+			nazwa: 'pięć',
+			name: 'five',
+			image: 'images/unit2numbers/five.svg'
+		},
+		{
+			nazwa: 'sześć',
+			name: 'six',
+			image: 'images/unit2numbers/six.svg'
+		},
+		{
+			nazwa: 'siedem',
+			name: 'seven',
+			image: 'images/unit2numbers/seven.svg'
+		},
+		{
+			nazwa: 'osiem',
+			name: 'eight',
+			image: 'images/unit2numbers/eight.svg'
+		},
+		{
+			nazwa: 'dziewięć',
+			name: 'nine',
+			image: 'images/unit2numbers/nine.svg'
+		},
+		{
+			nazwa: 'dziesięć',
+			name: 'ten',
+			image: 'images/unit2numbers/ten.svg'
+		},
 	],
 
-	[
-		{ nazwaZbioru: 'Unit 2. Liczby', opis: 'Liczby', polecenie: 'Naucz się cyferek', image: 'images/numbers.jpg' },
-	]
+	[{
+		nazwaZbioru: 'Unit 2. Liczby',
+		opis: 'Cyfry',
+		polecenie: 'Naucz się cyferek',
+		image: 'images/numbers.jpg'
+	}, ]
 ];
 
 
 let u2zwierzeta = [
 
-	[
-		{ nazwa: 'tygrysek', name: 'baby tiger', image: 'images/unit2africananimals/baby_tiger.svg' },
-		{ nazwa: 'słoń', name: 'elephant', image: 'images/unit2africananimals/elephant.svg' },
-		{ nazwa: 'żyrafa', name: 'giraffe', image: 'images/unit2africananimals/giraffe.svg' },
-		{ nazwa: 'hipopotam', name: 'hippo', image: 'images/unit2africananimals/hippo.svg' },
-		{ nazwa: 'lampart', name: 'leopard', image: 'images/unit2africananimals/leopard.svg' },
-		{ nazwa: 'nosorożec', name: 'rhino', image: 'images/unit2africananimals/rhino.svg' },
-		{ nazwa: 'tygrys', name: 'tiger', image: 'images/unit2africananimals/tiger.svg' },
-		{ nazwa: 'zebra', name: 'zebra', image: 'images/unit2africananimals/zebra.svg' },
+	[{
+			nazwa: 'tygrysek',
+			name: 'baby tiger',
+			image: 'images/unit2africananimals/baby_tiger.svg'
+		},
+		{
+			nazwa: 'słoń',
+			name: 'elephant',
+			image: 'images/unit2africananimals/elephant.svg'
+		},
+		{
+			nazwa: 'żyrafa',
+			name: 'giraffe',
+			image: 'images/unit2africananimals/giraffe.svg'
+		},
+		{
+			nazwa: 'hipopotam',
+			name: 'hippo',
+			image: 'images/unit2africananimals/hippo.svg'
+		},
+		{
+			nazwa: 'lampart',
+			name: 'leopard',
+			image: 'images/unit2africananimals/leopard.svg'
+		},
+		{
+			nazwa: 'nosorożec',
+			name: 'rhino',
+			image: 'images/unit2africananimals/rhino.svg'
+		},
+		{
+			nazwa: 'tygrys',
+			name: 'tiger',
+			image: 'images/unit2africananimals/tiger.svg'
+		},
+		{
+			nazwa: 'zebra',
+			name: 'zebra',
+			image: 'images/unit2africananimals/zebra.svg'
+		},
 	],
 
-	[
-		{ nazwaZbioru: 'Unit 2. Zwierzęta afrykańskie', opis: 'Zwierzęta afrykańskie', polecenie: 'Poznaj zwierzęta egzotycne', image: 'images/africananimals.jpg' },
-	]
+	[{
+		nazwaZbioru: 'Unit 2. Zwierzęta afrykańskie',
+		opis: 'Zwierzęta afrykańskie',
+		polecenie: 'Poznaj zwierzęta egzotycne',
+		image: 'images/africananimals.jpg'
+	}, ]
 ];
 
 
 let u6Dom = [
-	
-	[
-	{ nazwa: 'łazienka', name : 'bathroom', image: 'images/unit6/bathroom.svg' },
-	{ nazwa: 'sypialnia', name : 'bedroom', image: 'images/unit6/bedroom.svg' }, 
-	{ nazwa: 'ogród', name : 'garden', image: 'images/unit6/garden.svg' },
-	{ nazwa: 'dom', name : 'house', image: 'images/unit6/house.svg' },
-	{ nazwa: 'kuchnia', name : 'kitchen', image: 'images/unit6/kitchen.svg' },
-	{ nazwa: 'duży pokój', name : 'living room', image: 'images/unit6/livingroom.svg' },
-	{ nazwa: 'krzesło', name : 'chair', image: 'images/unit6/chair.svg' },
-	{ nazwa: 'drzwi', name : 'door', image: 'images/unit6/door.svg' },
-	{ nazwa: 'stół', name : 'table', image: 'images/unit6/table.svg' },	
-	{ nazwa: 'okno', name : 'window', image: 'images/unit6/window.svg' },
-	],
-	
-	[
-	{ nazwaZbioru: 'Unit 6. Dom', opis: 'Dom', polecenie: 'Pomieszczenia i wyposażenie', image: 'images/home.jpg' },
-	]
-];
-	
-let u7Clothes = [
-	
-	[
-	{ nazwa: 'but', name : 'boot', image: 'images/unit7/boots.svg' },
-	{ nazwa: 'płaszcz', name : 'coat', image: 'images/unit7/coat.svg' }, 
-	{ nazwa: 'sukienka', name : 'dress', image: 'images/unit7/dress.svg' },
-	{ nazwa: 'kapelusz', name : 'hat', image: 'images/unit7/hat.svg' },
-	{ nazwa: 'koszula', name : 'shirt', image: 'images/unit7/shirt.svg' },
-	{ nazwa: 'but', name : 'shoe', image: 'images/unit7/shoes.svg' },
-	{ nazwa: 'spódnica', name : 'skirt', image: 'images/unit7/skirt.svg' },
-	{ nazwa: 'sweter', name : 'sweater', image: 'images/unit7/sweater.svg' },
-	{ nazwa: 'spodnie', name : 'trousers', image: 'images/unit7/trousers.svg' },	
-	{ nazwa: 'podkoszulek', name : 't-shirt', image: 'images/unit7/T-shirt.svg' },
-	{ nazwa: 'skarpetki', name : 'socks', image: 'images/unit7/socks.svg' },
-	{ nazwa: 'szalik', name : 'scarf', image: 'images/unit7/scarf.svg' },
-	{ nazwa: 'buty zimowe', name : 'winter shoes', image: 'images/unit7/winter_shoes.svg' },
-	{ nazwa: 'kurtka zimowa', name : 'winter coat', image: 'images/unit7/winter_coat.svg' },	
+
+	[{
+			nazwa: 'łazienka',
+			name: 'bathroom',
+			image: 'images/unit6/bathroom.svg'
+		},
+		{
+			nazwa: 'sypialnia',
+			name: 'bedroom',
+			image: 'images/unit6/bedroom.svg'
+		},
+		{
+			nazwa: 'ogród',
+			name: 'garden',
+			image: 'images/unit6/garden.svg'
+		},
+		{
+			nazwa: 'dom',
+			name: 'house',
+			image: 'images/unit6/house.svg'
+		},
+		{
+			nazwa: 'kuchnia',
+			name: 'kitchen',
+			image: 'images/unit6/kitchen.svg'
+		},
+		{
+			nazwa: 'duży pokój',
+			name: 'living room',
+			image: 'images/unit6/livingroom.svg'
+		},
+		{
+			nazwa: 'krzesło',
+			name: 'chair',
+			image: 'images/unit6/chair.svg'
+		},
+		{
+			nazwa: 'drzwi',
+			name: 'door',
+			image: 'images/unit6/door.svg'
+		},
+		{
+			nazwa: 'stół',
+			name: 'table',
+			image: 'images/unit6/table.svg'
+		},
+		{
+			nazwa: 'okno',
+			name: 'window',
+			image: 'images/unit6/window.svg'
+		},
 	],
 
-	
-	[
-	{ nazwaZbioru: 'Unit 7. Ubrania', opis: 'Ubrania', polecenie: 'Naucz się nazw ubrań', image: 'images/dress.jpg' },
-	]
-	
+	[{
+		nazwaZbioru: 'Unit 6. Dom',
+		opis: 'Dom',
+		polecenie: 'Pomieszczenia i wyposażenie',
+		image: 'images/home.jpg'
+	}, ]
+];
+
+let u7Clothes = [
+
+	[{
+			nazwa: 'but',
+			name: 'boot',
+			image: 'images/unit7/boots.svg'
+		},
+		{
+			nazwa: 'płaszcz',
+			name: 'coat',
+			image: 'images/unit7/coat.svg'
+		},
+		{
+			nazwa: 'sukienka',
+			name: 'dress',
+			image: 'images/unit7/dress.svg'
+		},
+		{
+			nazwa: 'kapelusz',
+			name: 'hat',
+			image: 'images/unit7/hat.svg'
+		},
+		{
+			nazwa: 'koszula',
+			name: 'shirt',
+			image: 'images/unit7/shirt.svg'
+		},
+		{
+			nazwa: 'but',
+			name: 'shoe',
+			image: 'images/unit7/shoes.svg'
+		},
+		{
+			nazwa: 'spódnica',
+			name: 'skirt',
+			image: 'images/unit7/skirt.svg'
+		},
+		{
+			nazwa: 'sweter',
+			name: 'sweater',
+			image: 'images/unit7/sweater.svg'
+		},
+		{
+			nazwa: 'spodnie',
+			name: 'trousers',
+			image: 'images/unit7/trousers.svg'
+		},
+		{
+			nazwa: 'podkoszulek',
+			name: 't-shirt',
+			image: 'images/unit7/T-shirt.svg'
+		},
+		{
+			nazwa: 'skarpetki',
+			name: 'socks',
+			image: 'images/unit7/socks.svg'
+		},
+		{
+			nazwa: 'szalik',
+			name: 'scarf',
+			image: 'images/unit7/scarf.svg'
+		},
+		{
+			nazwa: 'buty zimowe',
+			name: 'winter shoes',
+			image: 'images/unit7/winter_shoes.svg'
+		},
+		{
+			nazwa: 'kurtka zimowa',
+			name: 'winter coat',
+			image: 'images/unit7/winter_coat.svg'
+		},
+	],
+
+
+	[{
+		nazwaZbioru: 'Unit 7. Ubrania',
+		opis: 'Ubrania',
+		polecenie: 'Naucz się nazw ubrań',
+		image: 'images/dress.jpg'
+	}, ]
+
 ];
 
 let u8Animals = [
-	
-	[
-	{ nazwa: 'ptak', name : 'bird', image: 'images/unit8/bird.svg' },
-	{ nazwa: 'kot', name : 'cat', image: 'images/unit8/cat.svg' }, 
-	{ nazwa: 'pies', name : 'dog', image: 'images/unit8/dog.svg' },
-	{ nazwa: 'kaczka', name : 'duck', image: 'images/unit8/duck.svg' },
-	{ nazwa: 'ryba', name : 'fish', image: 'images/unit8/fish.svg' },
-	{ nazwa: 'chomik', name : 'hamster', image: 'images/unit8/hamster.svg' },
-	{ nazwa: 'koń', name : 'horse', image: 'images/unit8/horse.svg' },
-	{ nazwa: 'mysz', name : 'mouse', image: 'images/unit8/mouse.svg' },
-	{ nazwa: 'królik', name : 'rabbit', image: 'images/unit8/rabbit.svg' },	
-	{ nazwa: 'żółw', name : 'tortoise', image: 'images/unit8/tortoise.svg' },
+
+	[{
+			nazwa: 'ptak',
+			name: 'bird',
+			image: 'images/unit8/bird.svg'
+		},
+		{
+			nazwa: 'kot',
+			name: 'cat',
+			image: 'images/unit8/cat.svg'
+		},
+		{
+			nazwa: 'pies',
+			name: 'dog',
+			image: 'images/unit8/dog.svg'
+		},
+		{
+			nazwa: 'kaczka',
+			name: 'duck',
+			image: 'images/unit8/duck.svg'
+		},
+		{
+			nazwa: 'ryba',
+			name: 'fish',
+			image: 'images/unit8/fish.svg'
+		},
+		{
+			nazwa: 'chomik',
+			name: 'hamster',
+			image: 'images/unit8/hamster.svg'
+		},
+		{
+			nazwa: 'koń',
+			name: 'horse',
+			image: 'images/unit8/horse.svg'
+		},
+		{
+			nazwa: 'mysz',
+			name: 'mouse',
+			image: 'images/unit8/mouse.svg'
+		},
+		{
+			nazwa: 'królik',
+			name: 'rabbit',
+			image: 'images/unit8/rabbit.svg'
+		},
+		{
+			nazwa: 'żółw',
+			name: 'tortoise',
+			image: 'images/unit8/tortoise.svg'
+		},
 	],
-	
-	[
-	{ nazwaZbioru: 'Unit 8. Zwierzęta', opis: 'Zwierzęta', polecenie: 'Naucz się nazw zwierząt', image: 'images/dog.jpg' },
-	]
-	
+
+	[{
+		nazwaZbioru: 'Unit 8. Zwierzęta',
+		opis: 'Zwierzęta',
+		polecenie: 'Naucz się nazw zwierząt',
+		image: 'images/dog.jpg'
+	}, ]
+
 ];
 
-let elementsMag = [owoce, kolory, u2zwierzeta, u2liczby, u6Dom, u7Clothes, u8Animals];
+let numbers1120 = [
+
+	[{
+			nazwa: 'jedenście',
+			name: 'eleven',
+			image: 'images/numbers1120/eleven.svg'
+		},
+		{
+			nazwa: 'dwanaście',
+			name: 'twelve',
+			image: 'images/numbers1120/twelve.svg'
+		},
+		{
+			nazwa: 'trzynaście',
+			name: 'thirteen',
+			image: 'images/numbers1120/thirteen.svg'
+		},
+		{
+			nazwa: 'czternaście',
+			name: 'fourteen',
+			image: 'images/numbers1120/fourteen.svg'
+		},
+		{
+			nazwa: 'piętnaście',
+			name: 'fifteen',
+			image: 'images/numbers1120/fifteen.svg'
+		},
+		{
+			nazwa: 'szesnaście',
+			name: 'sixteen',
+			image: 'images/numbers1120/sixteen.svg'
+		},
+		{
+			nazwa: 'siedemnaście',
+			name: 'seventeen',
+			image: 'images/numbers1120/seventeen.svg'
+		},
+		{
+			nazwa: 'osiemnaście',
+			name: 'eighteen',
+			image: 'images/numbers1120/eighteen.svg'
+		},
+		{
+			nazwa: 'dziewiętnaście',
+			name: 'nineteen',
+			image: 'images/numbers1120/nineteen.svg'
+		},
+		{
+			nazwa: 'dwadzieścia',
+			name: 'twenty',
+			image: 'images/numbers1120/twenty.svg'
+		},
+	],
+
+	[{
+		nazwaZbioru: 'Liczby 11 - 20',
+		opis: 'Liczby od 11 do 20',
+		polecenie: 'Naucz się liczyć do 20',
+		image: 'images/numbers1120.jpg'
+	}, ]
+
+];
+
+let elementsMag = [owoce, kolory, u2zwierzeta, u2liczby, u6Dom, u7Clothes, u8Animals, numbers1120];
 
 let pushbuttons = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż'];
